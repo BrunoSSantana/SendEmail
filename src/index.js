c27d2524e01b026333cdcd9c45d5343a48edb78c
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-24 15:29:46 -0300

    :art: separate routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const nodemailer = require('nodemailer');
 const methodOverride = require('method-override');
-const SMTP_CONFIG = require('./config/smtp');
+const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
@@ -10,39 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(methodOverride('_method')); // antes das rotas smp
-
-app.get('/', (request, response) => response.render('index.html'));
-
-app.post('/', (request, response) => {
-  const { to, name } = request.body;
-  console.log(to);
-  console.log(name);
-
-  const transporter = nodemailer.createTransport({
-    host: SMTP_CONFIG.host,
-    port: SMTP_CONFIG.port,
-    secure: false,
-    auth: {
-      user: SMTP_CONFIG.user,
-      pass: SMTP_CONFIG.pass,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  async function run() {
-    await transporter.sendMail({
-      text: 'Texto do Email',
-      subject: 'Assunto do e-mail',
-      from: 'Bruno Souza <brunoosouza15@gmail.com>',
-      to: ['brunoosouza15@gmail.com', 'brunoosouzas@gmail.com'],
-    });
-  }
-
-  run();
-  return response.send();
-});
+app.use(routes);
 
 app.set('view engine', 'njk');
 
