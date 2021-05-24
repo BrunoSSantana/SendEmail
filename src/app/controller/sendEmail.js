43e829018c241d76f2203bc81f9b78bba123e377
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-24 15:39:47 -0300

    :art: separate controller

---
+++ src/app/controller/sendEmail.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+const SMTP_CONFIG = require('../../config/smtp');
+
+module.exports = {
+  index(request, response) { response.render('index.html'); },
+  post(request, response) {
+    const { to, name } = request.body;
+    console.log(to);
+    console.log(name);
+
+    const transporter = nodemailer.createTransport({
+      host: SMTP_CONFIG.host,
+      port: SMTP_CONFIG.port,
+      secure: false,
+      auth: {
+        user: SMTP_CONFIG.user,
+        pass: SMTP_CONFIG.pass,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    async function run() {
+      await transporter.sendMail({
+        text: 'Texto do Email',
+        subject: 'Assunto do e-mail',
+        from: 'Bruno Souza <brunoosouza15@gmail.com>',
+        to: ['brunoosouza15@gmail.com', 'brunoosouzas@gmail.com'],
+      });
+    }
+
+    run();
+    return response.send();
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,40 +1,10 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const SMTP_CONFIG = require('./config/smtp');
+const sendEmail = require('./app/controller/sendEmail');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => response.render('index.html'));
+routes.get('/', sendEmail.index);
 
-routes.post('/', (request, response) => {
-  const { to, name } = request.body;
-  console.log(to);
-  console.log(name);
-
-  const transporter = nodemailer.createTransport({
-    host: SMTP_CONFIG.host,
-    port: SMTP_CONFIG.port,
-    secure: false,
-    auth: {
-      user: SMTP_CONFIG.user,
-      pass: SMTP_CONFIG.pass,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  async function run() {
-    await transporter.sendMail({
-      text: 'Texto do Email',
-      subject: 'Assunto do e-mail',
-      from: 'Bruno Souza <brunoosouza15@gmail.com>',
-      to: ['brunoosouza15@gmail.com', 'brunoosouzas@gmail.com'],
-    });
-  }
-
-  run();
-  return response.send();
-});
+routes.post('/', sendEmail.post);
 
 module.exports = routes;
