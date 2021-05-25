11276aa75fa87414d44cfefe620281add0e8f7af
BrunoSSantana <brunoosouza15@gmail.com>
2021-05-25 10:15:45 -0300

    :hammer: finishing

--- src/app/controller/listUsers.js
+++ src/app/controller/listUsers.js
@@ -3,7 +3,7 @@ const User = require('../model/User');
 module.exports = {
   async list(request, response) {
     const users = await User.find({});
-    console.log(users);
+
     return response.render('list.njk', { users });
   },
 };
